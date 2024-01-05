b2cee3557d212000d1adecac696b1f70423439f0
Guilherme Frainer <gui.sf.2012@gmail.com>
2024-01-05 12:15:46 -0300

    Configuração final dos modelos

---
+++ forecasting_vars.py
@@ -0,0 +1,26 @@
+# Parâmetros gerais
+TEST_SIZE = 79
+LINEAR_MODELS = ['ridge', 'lasso'] # Modelos cujos dados precisam ser normalizados
+WINDOW_LENGTH = 12
+N_JOBS = 4
+FIGSIZE = (10, 5)
+
+# Parâmetros de modelos
+lasso = {
+    'max_iter': 2000,
+    'n_jobs': N_JOBS
+}
+
+rf = {
+    'n_estimators': 1000,
+    'max_features': 1.0,
+    'n_jobs': N_JOBS
+}
+
+lgbm = {
+    'n_jobs': N_JOBS,
+    'verbose': -1,
+    'force_col_wise': True,
+    'n_estimators': 1000,
+    'learning_rate': 0.01,
+}
